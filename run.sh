4b6abc29f0c63f05bfc078308f3d361e880e00b7
John Else <john.else@citrix.com>
2015-05-01 09:32:46 +0100

    Make a new mount directory for each SRPM
    Signed-off-by: John Else <john.else@citrix.com>

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ then
     exit 1
 fi
 
-SRPMS_MOUNT_DIR=/tmp/docker-SRPMS
+SRPMS_MOUNT_DIR=/tmp/docker-SRPMS/`uuidgen`
 
 SRPM=$1
 SRPM_NAME=`basename $SRPM`
 
-mkdir -p /tmp/docker-SRPMS
+mkdir -p $SRPMS_MOUNT_DIR
 cp $SRPM $SRPMS_MOUNT_DIR
 
 docker run -e SRPM_NAME=$SRPM_NAME -i --rm=true -t -v $SRPMS_MOUNT_DIR:/mnt/docker-SRPMS xenserver/xenserver-build-env
