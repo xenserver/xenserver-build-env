d363a13f283aad76036878c8d0579b23a9783474
John Else <john.else@gmail.com>
2018-08-06 01:54:09 +0100

    Build a repo from xcp-ng

--- test/travis-test.sh
+++ test/travis-test.sh
@@ -4,10 +4,10 @@ set -eux
 
 ./build.sh dev
 
-PACKAGE=xcp-networkd
-REPO=xcp-networkd
+PACKAGE=emu-manager
+REPO=xcp-emu-manager
 
-git clone git://github.com/xapi-project/$REPO /tmp/$REPO
+git clone git://github.com/xcp-ng/$REPO /tmp/$REPO
 
 cp \
     run.py \
@@ -20,5 +20,5 @@ cd /tmp/$REPO
 REPO_PACKAGE_NAME=$PACKAGE \
     REPO_CONFIGURE_CMD=true \
     REPO_BUILD_CMD=make \
-    REPO_TEST_CMD='make test' \
+    REPO_TEST_CMD=true\
     bash travis-build-repo.sh
