ed72d14ef1d467a9811da0625a9f2265237cb959
BenjiReis <benjamin.reis@vates.fr>
2021-11-23 11:47:58 +0100

    test
    Signed-off-by: BenjiReis <benjamin.reis@vates.fr>

--- test/test.sh
+++ test/test.sh
@@ -12,8 +12,11 @@ for REPO in ${REPOS}; do
     REPO_PATH=/tmp/"$REPO"
     git clone --branch "$TARGET_XCP_NG_VERSION" git://github.com/xcp-ng-rpms/"$REPO" "$REPO_PATH"
 
-    TARGET_XCP_NG_VERSION="$TARGET_XCP_NG_VERSION" \
-        bash utils/build-repo.sh "$REPO_PATH"
+    python run.py --name "$CONTAINER_NAME" \
+        --fail-on-error \
+        -l "$REPO_PATH" \
+        -b "$TARGET_XCP_NG_VERSION" \
+        --rm
 
     rm -rf "$REPO_PATH"
 done
